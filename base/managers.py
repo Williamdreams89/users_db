cfb08743fa0ef1d5896993da1500dd48dfd2942e
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2022-09-27 12:55:39 +0000

    Created the customed User

---
+++ base/managers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, department, role, password=None):
+        if not email:
+            raise ValueError("Email field cannot be blank")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, department=department, role=role, password=password)
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user 
+
+
+        
--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    role= models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "department"]
+
+
